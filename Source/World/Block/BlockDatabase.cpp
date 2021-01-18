5b66d39ab13c642ffdc4e9fb2a510eb7fa86329d
adjsky <adjsky@DESKTOP-O1MLU1T>
2021-01-18 08:01:50 +0300

    bug fix

--- Source/World/Block/BlockDatabase.cpp
+++ Source/World/Block/BlockDatabase.cpp
@@ -5,15 +5,16 @@
 #include <algorithm>
 
 #include "BlockDatabase.h"
+#include "../../Util/constants.h"
 
 std::array<std::unique_ptr<BlockData>, BlockType::TYPE_COUNT> BlockDatabase::blocksData{};
 
 void BlockDatabase::initialize() {
     std::generate(blocksData.begin(), blocksData.end(), []() { return std::make_unique<BlockData>(); });
     for (std::size_t i = 0; i < blocksData.size(); i++) {
-        blocksData[i]->textureRect.width = 96;
-        blocksData[i]->textureRect.height = 96;
-        blocksData[i]->textureRect.left = 96 * i;
+        blocksData[i]->textureRect.width = 64;
+        blocksData[i]->textureRect.height = 64;
+        blocksData[i]->textureRect.left = 64 * i;
     }
 }
 
