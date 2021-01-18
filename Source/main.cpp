a28a82809018dfffd4555c75b3962e6899279fba
adjsky <adjsky@DESKTOP-O1MLU1T>
2021-01-18 08:00:44 +0300

    add context settings

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,10 @@
 #include "ResourceManager/ResourceManager.h"
-#include "World/World.h"
 #include "Game/Game.h"
 
 int main() {
-    ResourceManager::initialize();
-    World::initialize();
-    Game game{};
+    ResourceManager::initializeTextures();
+    sf::ContextSettings context{};
+    Game game{ context };
     game.start();
     return 0;
 }
