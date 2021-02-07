9268ec35ea457412b6c8d1018a9f5f6426794722
adjsky <adjsky@DESKTOP-O1MLU1T>
2021-02-07 15:07:50 +1000

    separate header definition into .h/.cpp file

--- Source/Physics/Collisions.h
+++ Source/Physics/Collisions.h
@@ -7,41 +7,9 @@
 #include "../Util/utility.h"
 #include "../Maths/Math.h"
 
-#include <iostream>
-
 #ifndef TERRARIA_CLONE_COLLISIONS_H
 #define TERRARIA_CLONE_COLLISIONS_H
 
-bool canPlaceBlock(const Player& player, const sf::Vector2i& pos, const World& world) {
-    sf::FloatRect playerHitBox { player.getHitBox().getGlobalBounds() };
-    sf::Vector2f startPos { mapGlobalCoordsToGame(playerHitBox.left, playerHitBox.top + playerHitBox.height) };
-    sf::Vector2f endPos { mapGlobalCoordsToGame(playerHitBox.left + playerHitBox.width, playerHitBox.top) };
-    const auto* block{ world.getBlock(pos.x, pos.y) };
-    if (block) {
-        if (block->visible) return false;
-    }
-    if (pos.x >= startPos.x && pos.x <= endPos.x && pos.y >= startPos.y && pos.y <= endPos.y) {
-        return false;
-    } else {
-        const auto* leftBlock { world.getBlock(pos.x - 1, pos.y) };
-        if (leftBlock) {
-            if (leftBlock->visible) return true;
-        }
-        const auto* rightBlock { world.getBlock(pos.x + 1, pos.y) };
-        if (rightBlock) {
-            if (rightBlock->visible) return true;
-        }
-        const auto* topBlock { world.getBlock(pos.x, pos.y + 1) };
-        if (topBlock) {
-            if (topBlock->visible) return true;
-        }
-        const auto* bottomBlock { world.getBlock(pos.x, pos.y - 1) };
-        if (bottomBlock) {
-            if (bottomBlock->visible) return true;
-        }
-
-        return false;
-    }
-}
+bool canPlaceBlock(const Player& player, const sf::Vector2i& pos, const World& world);
 
 #endif //TERRARIA_CLONE_COLLISIONS_H
